258d6eebd930c20f289212d5ea1456889f506436
deemon <deemon@k66.ru>
2020-10-12 16:32:15 +0500

    2

--- 1/Form1.Designer.cs
+++ 1/Form1.Designer.cs
@@ -29,7 +29,6 @@
 		private void InitializeComponent()
 		{
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
@@ -43,30 +42,24 @@
             this.radioButton6 = new System.Windows.Forms.RadioButton();
             this.radioButton7 = new System.Windows.Forms.RadioButton();
             this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(66, 24);
+            this.button1.Location = new System.Drawing.Point(58, 376);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 39);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Показать тарифы";
+            this.button1.Text = "Подключить тариф и опции";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(66, 69);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(66, 108);
+            this.radioButton1.Location = new System.Drawing.Point(18, 3);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(79, 17);
             this.radioButton1.TabIndex = 3;
@@ -78,7 +71,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(66, 131);
+            this.radioButton2.Location = new System.Drawing.Point(18, 26);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(79, 17);
             this.radioButton2.TabIndex = 4;
@@ -89,7 +82,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(66, 294);
+            this.checkBox1.Location = new System.Drawing.Point(58, 236);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(73, 17);
             this.checkBox1.TabIndex = 5;
@@ -100,47 +93,51 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(66, 317);
+            this.checkBox2.Location = new System.Drawing.Point(58, 259);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(153, 17);
             this.checkBox2.TabIndex = 6;
             this.checkBox2.Text = "Переадрессация вызова";
             this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(66, 340);
+            this.checkBox3.Location = new System.Drawing.Point(58, 282);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(113, 17);
             this.checkBox3.TabIndex = 7;
             this.checkBox3.Text = "Автоинформатор";
             this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
             // 
             // checkBox5
             // 
             this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(66, 387);
+            this.checkBox5.Location = new System.Drawing.Point(58, 329);
             this.checkBox5.Name = "checkBox5";
             this.checkBox5.Size = new System.Drawing.Size(69, 17);
             this.checkBox5.TabIndex = 9;
             this.checkBox5.Text = "Локатор";
             this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
             // 
             // checkBox6
             // 
             this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(66, 411);
+            this.checkBox6.Location = new System.Drawing.Point(58, 353);
             this.checkBox6.Name = "checkBox6";
             this.checkBox6.Size = new System.Drawing.Size(113, 17);
             this.checkBox6.TabIndex = 10;
             this.checkBox6.Text = "Запрет подписок";
             this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(66, 154);
+            this.radioButton3.Location = new System.Drawing.Point(18, 49);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(79, 17);
             this.radioButton3.TabIndex = 11;
@@ -152,7 +149,7 @@
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(66, 177);
+            this.radioButton4.Location = new System.Drawing.Point(18, 72);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(79, 17);
             this.radioButton4.TabIndex = 12;
@@ -163,7 +160,7 @@
             // radioButton5
             // 
             this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(66, 200);
+            this.radioButton5.Location = new System.Drawing.Point(18, 95);
             this.radioButton5.Name = "radioButton5";
             this.radioButton5.Size = new System.Drawing.Size(79, 17);
             this.radioButton5.TabIndex = 13;
@@ -174,7 +171,7 @@
             // radioButton6
             // 
             this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(66, 223);
+            this.radioButton6.Location = new System.Drawing.Point(18, 118);
             this.radioButton6.Name = "radioButton6";
             this.radioButton6.Size = new System.Drawing.Size(79, 17);
             this.radioButton6.TabIndex = 14;
@@ -186,7 +183,8 @@
             // radioButton7
             // 
             this.radioButton7.AutoSize = true;
-            this.radioButton7.Location = new System.Drawing.Point(66, 246);
+            this.radioButton7.Checked = true;
+            this.radioButton7.Location = new System.Drawing.Point(18, 141);
             this.radioButton7.Name = "radioButton7";
             this.radioButton7.Size = new System.Drawing.Size(79, 17);
             this.radioButton7.TabIndex = 15;
@@ -197,36 +195,46 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(66, 363);
+            this.checkBox4.Location = new System.Drawing.Point(58, 305);
             this.checkBox4.Name = "checkBox4";
             this.checkBox4.Size = new System.Drawing.Size(155, 17);
             this.checkBox4.TabIndex = 16;
             this.checkBox4.Text = "Расширенный пакет СМС";
             this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton7);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton6);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Location = new System.Drawing.Point(40, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(238, 180);
+            this.panel1.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.radioButton7);
-            this.Controls.Add(this.radioButton6);
-            this.Controls.Add(this.radioButton5);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.checkBox6);
             this.Controls.Add(this.checkBox5);
             this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -235,7 +243,6 @@
 		#endregion
 
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.RadioButton radioButton1;
 		private System.Windows.Forms.RadioButton radioButton2;
 		private System.Windows.Forms.CheckBox checkBox1;
@@ -249,6 +256,7 @@
 		private System.Windows.Forms.RadioButton radioButton6;
 		private System.Windows.Forms.RadioButton radioButton7;
         private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- 1/Form1.cs
+++ 1/Form1.cs
@@ -10,56 +10,101 @@ using System.Windows.Forms;
 
 namespace _1
 {
+    
 	public partial class Form1 : Form
 	{
 		public Form1()
 		{
 			InitializeComponent();
 		}
-
 		private void Form1_Load(object sender, EventArgs e)
 		{
 
 		}
-
 		private void button1_Click(object sender, EventArgs e)
 		{
-			textBox1.Text = "Hello World!";
-		}
+			string tarif = null;
+			string option = null;
 
+			if (radioButton1.Checked) tarif = "100";
+			if (radioButton2.Checked) tarif = "200";
+			if (radioButton3.Checked) tarif = "300";
+			if (radioButton4.Checked) tarif = "400";
+			if (radioButton5.Checked) tarif = "500";
+			if (radioButton6.Checked) tarif = "600";
+			if (radioButton7.Checked) tarif = "700";
+			if (checkBox1.Checked) option = "АнтиАОН" + "\n";
+			if (checkBox2.Checked) option = option + "Переадрессация вызова" + "\n";
+			if (checkBox3.Checked) option = option + "Автоинформатор" + "\n";
+			if (checkBox4.Checked) option = option + "Расширенный пакет СМС" + "\n";
+			if (checkBox5.Checked) option = option + "Локатор" + "\n";
+			if (checkBox6.Checked) option = option + "Запрет подписок" + "\n";
+
+			MessageBox.Show("Вы подключили тариф " + tarif + " c опциями:\n" + option);	
+		}
 		private void textBox1_TextChanged(object sender, EventArgs e)
 		{
 
 		}
-
 		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
 		{
 
 		}
-
 		private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
 		{
 
 		}
-
 		private void radioButton1_CheckedChanged(object sender, EventArgs e)
 		{
 
 		}
+		private void radioButton2_CheckedChanged(object sender, EventArgs e)
+		{
 
+		}
 		private void radioButton3_CheckedChanged(object sender, EventArgs e)
 		{
 
 		}
+		private void radioButton4_CheckedChanged(object sender, EventArgs e)
+		{
 
+		}
+		private void radioButton5_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
 		private void radioButton6_CheckedChanged(object sender, EventArgs e)
 		{
 
 		}
-
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
+		private void checkBox2_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+		private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
